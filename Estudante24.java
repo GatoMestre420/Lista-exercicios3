83a0386e18f878326099dca710030ad9ed4cb1f3
GatoMestre420 <luancurt@outlook.com>
2024-03-25 10:50:23 -0300

    ex24

---
+++ Estudante24.java
@@ -0,0 +1,18 @@
+public class Estudante24 {
+
+    private double notaLaboratorio;
+    private double notaAvaliacaoSemestral;
+    private double notaExameFinal;
+
+    public Estudante24(double notaLaboratorio, double notaAvaliacaoSemestral, double notaExameFinal) {
+        this.notaLaboratorio = notaLaboratorio;
+        this.notaAvaliacaoSemestral = notaAvaliacaoSemestral;
+        this.notaExameFinal = notaExameFinal;
+    }
+
+    public double calcularNotaFinal() {
+        double notaFinal = (notaLaboratorio * 2 + notaAvaliacaoSemestral * 3 + notaExameFinal * 5) / 10;
+        return notaFinal;
+    }
+
+}
