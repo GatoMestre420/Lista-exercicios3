fc0a44f348f99c918444b5ab6061304bbe42bdef
GatoMestre420 <luancurt@outlook.com>
2024-03-25 10:41:38 -0300

    ex10

---
+++ Pessoas.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Pessoas {
+    int idade;
+
+    public Pessoas(int idade) {
+        this.idade = idade;
+    }
+
+    public void verificarIdade() {
+        if (idade >= 18) {
+            System.out.println("Maior de idade");
+        } else {
+            System.out.println("Menor de idade");
+        }
+    }
+}
