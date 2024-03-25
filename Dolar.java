1ece379335111c61e3591299002f20e1d0be5816
GatoMestre420 <luancurt@outlook.com>
2024-03-25 10:29:54 -0300

    ex4

---
+++ Dolar.java
@@ -0,0 +1,10 @@
+public class Dolar {
+
+    public double quant;
+    public double valorCot;
+
+    public double calcCot(){
+        return quant * valorCot;
+    }
+
+}
---
+++ Ex04.java
@@ -0,0 +1,24 @@
+// 4. Elaborar um programa que efetue a apresentação do valor da conversão em real (R$) de umvalor lido em dólar (US$). O programa deverá solicitar o valor da cotação do dólar e também aquantidade de dólares disponíveis com o usuário
+
+import java.util.Scanner;
+
+public class Ex04 {
+    public static void executar(){
+        Scanner leitor = new Scanner(System.in);
+
+        Dolar dollar = new Dolar();
+
+        System.out.println("Infome o valor da cotação do dollar: ");
+        dollar.valorCot = Prompt.lerDecimal();
+        System.out.println("Informe a quantidade para conversão : ");
+        dollar.quant = Prompt.lerDecimal();
+
+        System.out.println("O valor de $" + dollar.quant + " em reais é: " + dollar.calcCot() + "R$");
+
+
+
+
+        leitor.close();
+    }
+
+}
