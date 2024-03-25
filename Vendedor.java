a1410af626158d859152ac584fe741b17ac7b630
GatoMestre420 <luancurt@outlook.com>
2024-03-25 10:28:46 -0300

    ex3

---
+++ Vendedor.java
@@ -0,0 +1,12 @@
+public class Vendedor {
+
+    public String nome;
+    public double salarioFix;
+    public double totalVendas;
+
+    public double calcSalario(){
+
+        return salarioFix + (totalVendas * 0.15);
+    }
+
+}
