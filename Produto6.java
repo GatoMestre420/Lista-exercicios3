bad47d9dbe2d3cf875fb1adec4a51c2126d7b6d4
GatoMestre420 <luancurt@outlook.com>
2024-03-25 10:32:10 -0300

    ex6

---
+++ Produto6.java
@@ -0,0 +1,15 @@
+public class Produto6 {
+
+    public double precoCusto;
+    public double valorVenda;
+    public double acrescimo;
+    
+
+    public double calcValorDeVenda(){
+
+        valorVenda = precoCusto * ( 1 + acrescimo/100);
+        return(valorVenda);
+    }
+
+    
+}
